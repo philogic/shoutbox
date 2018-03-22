26db4ffc39d0c8f5df728fee2e504897d40ed0b3
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2018-03-23 00:07:42 +0100

    Add Dashboard Controller, set url after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    dashboard_show_path
+  end
+
   protected
 
   def configure_permitted_parameters
@@ -9,4 +13,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :password_confirmation])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'dashboard/show'
+
   root to: 'home#index'
   devise_for :users
 end
